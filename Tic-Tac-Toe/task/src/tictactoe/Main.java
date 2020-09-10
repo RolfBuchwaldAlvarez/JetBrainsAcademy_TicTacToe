438232f3aef3c0e2f8c136438b3d8a45777d6b57
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-09-10 12:03:29 +0200

    Upload project files

---
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -0,0 +1,12 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        char x = 'X';
+        char o = 'O';
+
+        System.out.println(x + " " + x + " " + o);
+        System.out.println(o + " " + o + " " + x);
+        System.out.println(o + " " + x + " " + x);
+    }
+}

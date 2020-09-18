a31d753e5bedda968aa491438a3285004be72788
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-09-18 11:36:38 +0200

    Add solved problem 'The minimum value of an array'

---
+++ Problems/The minimum value of an array/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = new int[scanner.nextInt()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int min = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+
+        System.out.println(min);
+    }
+}

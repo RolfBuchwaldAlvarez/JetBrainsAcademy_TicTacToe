bb6dec4408ad263f8059adb94a12a1b695fe4ddb
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-12-03 16:35:41 +0100

    Update problem solution for 'Spiral'

--- Problems/Spiral/src/Main.java
+++ Problems/Spiral/src/Main.java
@@ -1,5 +1,70 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+
+        fillOddSquareMatrix(n, matrix);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void fillOddSquareMatrix(int n, int[][] matrix) {
+        int matrixRowMax = n - 1;
+        int matrixColMax = n - 1;
+        int matrixRowMin = 0;
+        int matrixColMin = 0;
+        int direction = 1;
+        int num = 0;
+        int nSquare = n * n;
+
+        while (num < nSquare) {
+            // direction % 4 == 1: right
+            if (direction % 4 == 1 && num == 0) {
+                for (int col = matrixColMin; col <= matrixColMax; col++) {
+                    num++;
+                    matrix[matrixRowMin][col] = num;
+                }
+                matrixRowMin++;
+                direction++;
+            }
+            // direction % 4 == 1: right
+            if (direction % 4 == 1) {
+                for (int col = matrixColMin; col <= matrixColMax; col++) {
+                    num++;
+                    matrix[matrixRowMin][col] = num;
+                }
+                matrixRowMin++;
+                direction++;
+            } else if (direction % 4 == 2) { // direction % 4 == 2: down
+                for (int row = matrixRowMin; row <= matrixRowMax; row++) {
+                    num++;
+                    matrix[row][matrixColMax] = num;
+                }
+                matrixColMax--;
+                direction++;
+            } else if (direction % 4 == 3) { // direction % 4 == 3: left
+                for (int col = matrixColMax; col >= matrixColMin; col--) {
+                    num++;
+                    matrix[matrixRowMax][col] = num;
+                }
+                matrixRowMax--;
+                direction++;
+            } else if (direction % 4 == 0) { // direction % 4 == 0: up
+                for (int row = matrixRowMax; row >= matrixRowMin; row--) {
+                    num++;
+                    matrix[row][matrixColMin] = num;
+                }
+                matrixColMin++;
+                direction++;
+            }
+        }
     }
 }
